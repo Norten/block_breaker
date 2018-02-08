7ee885a56633f383a00ec1c44ca2532e54857862
Kamil <kuchciakkamil@gmail.com>
2018-02-08 20:20:36 +0100

    Initial commit

---
+++ Unity/Assets/Scripts/Ball.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Ball : MonoBehaviour {
+
+	private Paddle paddle;
+	private Vector3 ballToPaddleVector;
+	private bool ballFired = false;
+
+	// Use this for initialization
+	void Start () {
+		paddle = GameObject.FindObjectOfType<Paddle>();
+		ballToPaddleVector = this.transform.position - paddle.transform.position;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (!ballFired) {
+			this.transform.position = paddle.transform.position + ballToPaddleVector;
+			if (Input.GetMouseButtonDown(0)) {
+				print ("Mouse clicked");
+				ballFired = true;
+				this.rigidbody2D.velocity = new Vector2(6.0f, 6.0f);
+			}
+		}
+	}
+}
---
+++ Unity/Assets/Scripts/Paddle.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Paddle : MonoBehaviour {
+	
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		var mousePosInBlocks = Input.mousePosition.x / Screen.width * 16;
+		var paddlePos = gameObject.transform.position;
+		mousePosInBlocks = Mathf.Clamp(mousePosInBlocks, 0.5f, 15.5f);
+		gameObject.transform.position = new Vector3(mousePosInBlocks, paddlePos.y, paddlePos.z);
+	}
+}
